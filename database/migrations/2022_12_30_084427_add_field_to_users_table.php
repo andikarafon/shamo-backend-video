fd5ff2446cfa819b7415c615add05cae9faac3e8
andikarafon <andika.rafon.senohaji@gmail.com>
2023-01-02 14:59:16 +0700

    commit pertama

---
+++ database/migrations/2022_12_30_084427_add_field_to_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('roles')->after('email')->default('USER');
+            $table->string('phone')->after('email')->nullable();
+            $table->string('username')->after('email')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            // ini diisi karena nanti kalo akan ada rollback maka akan menghapus kolom yang ada
+            $table->dropColumn('roles');
+            $table->dropColumn('phone');
+            $table->dropColumn('username');
+        });
+    }
+};
